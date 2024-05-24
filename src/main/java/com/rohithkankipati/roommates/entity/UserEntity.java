ddf16104bd66aec2024861e610ac5f4c2f5e3e33
rohith4743 <rohithkankipati4743@gmail.com>
2024-05-24 00:32:29 -0400

    exception handling

---
+++ src/main/java/com/rohithkankipati/roommates/entity/UserEntity.java
@@ -0,0 +1,58 @@
+package com.rohithkankipati.roommates.entity;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "users")
+public class UserEntity {
+  
+  @Id
+  private String id;
+  private String username;
+  private String firstname;
+  private String lastname;
+  private String password;
+  
+  public String getId() {
+    return id;
+  }
+  
+  public void setId(String id) {
+    this.id = id;
+  }
+  
+  public String getUsername() {
+    return username;
+  }
+  
+  public void setUsername(String username) {
+    this.username = username;
+  }
+  
+  public String getFirstname() {
+    return firstname;
+  }
+  
+  public void setFirstname(String firstname) {
+    this.firstname = firstname;
+  }
+  
+  public String getLastname() {
+    return lastname;
+  }
+  
+  public void setLastname(String lastname) {
+    this.lastname = lastname;
+  }
+  
+  public String getPassword() {
+    return password;
+  }
+  
+  public void setPassword(String password) {
+    this.password = password;
+  }
+  
+  
+
+}
---
+++ src/test/java/com/rohithkankipati/roommates/RoommatesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rohithkankipati.roommates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoommatesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
